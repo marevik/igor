23b387b5fbf350d606bd99c7a664d799ff417e5a
Igor <igopko1996@gmail.com>
2020-09-22 22:20:53 +0300

    commit

--- script.js
+++ script.js
@@ -1115,3 +1115,32 @@
 //   );
 // }
 // console.log(getSecondsToTomorrow());
+
+//
+//
+
+//
+//--------------------JSON
+
+// let student = {
+//   name: 'John',
+//   age: 30,
+//   isAdmin: false,
+//   courses: ['html', 'css', 'js'],
+//   wife: null
+// };
+
+// let json = JSON.stringify(student);
+
+// alert(typeof json); // мы получили строку!
+
+// alert(json);
+// /* выведет объект в формате JSON:
+// {
+//   "name": "John",
+//   "age": 30,
+//   "isAdmin": false,
+//   "courses": ["html", "css", "js"],
+//   "wife": null
+// }
+// */
