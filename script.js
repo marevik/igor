c9497f67090ad52998061033f6db8a80298352d6
Igor <igopko1996@gmail.com>
2020-09-25 21:46:47 +0300

    commit

--- script.js
+++ script.js
@@ -1294,3 +1294,58 @@
 //   "place":{"number":23}
 // }
 // */
+
+//,//
+// //
+// //==================================================рекурсія
+//приклад
+// function pow(x, n) {
+//   let result = 1;
+//   // умножаем result на x n раз в цикле
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+//   return result;
+// }
+
+// alert(pow(2, 3)); // 8
+
+// ///приклад 2
+// function pow(x, n) {
+//   if (n == 1) {
+//     return x;
+//   } else {
+//     return x * pow(x, n - 1);
+//   }
+// }
+// alert(pow(2, 3)); // 8
+//
+//приклад 3
+// function pow(x, n) {
+//   return n == 1 ? x : x * pow(x, n - 1);
+// }
+// alert(pow(2, 3)); // 8
+//
+//
+// let company = { // тот же самый объект, сжатый для краткости
+//   sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600 }],
+//   development: {
+//     sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+//     internals: [{name: 'Jack', salary: 1300}]
+//   }
+// };
+
+// // Функция для подсчёта суммы зарплат
+// function sumSalaries(department) {
+//   if (Array.isArray(department)) { // случай (1)
+//     return department.reduce((prev, current) => prev + current.salary, 0); // сумма элементов массива
+//   } else { // случай (2)
+//     let sum = 0;
+//     for (let subdep of Object.values(department)) {
+//       sum += sumSalaries(subdep); // рекурсивно вызывается для подотделов, суммируя результаты
+//     }
+//     return sum;
+//   }
+// }
+
+// alert(sumSalaries(company)); // 6700
