22a222782ac63446074261dfb25d1cf0e82b8d09
Igor <igopko1996@gmail.com>
2020-10-07 23:43:01 +0300

    commit

--- script.js
+++ script.js
@@ -1566,3 +1566,53 @@
 // +(function () {
 //   alert("Выражение начинается с унарного плюса");
 // })();
+//
+//
+//
+
+// function sum(a) {
+//   return function (b) {
+//     return a + b; // берёт "a" из внешнего лексического окружения
+//   };
+// }
+
+// alert(sum(1)(2)); // 3
+// alert(sum(5)(-1)); // 4
+
+// function inBetween(a, b) {
+//   return function (x) {
+//     return x >= a && x <= b;
+//   };
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// alert(arr.filter(inBetween(3, 6))); // 3,4,5,6
+
+// function inArray(arr) {
+//   return function (x) {
+//     return arr.includes(x);
+//   };
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// alert(arr.filter(inArray([1, 2, 10]))); // 1,2
+
+// //
+// //
+// //
+// //сортування по полю
+// let users = [
+//   { name: "John", age: 20, surname: "Johnson" },
+//   { name: "Pete", age: 18, surname: "Peterson" },
+//   { name: "Ann", age: 19, surname: "Hathaway" },
+// ];
+
+// function byField(field) {
+//   return (a, b) => (a[field] > b[field] ? 1 : -1);
+// }
+
+// users.sort(byField("name"));
+// users.forEach((user) => alert(user.name)); // Ann, John, Pete
+
+// users.sort(byField("age"));
+// users.forEach((user) => alert(user.name)); // Pete, Ann, John
