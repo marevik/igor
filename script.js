672727837760fbeddd5b744c5e8607188f23fc77
Igor <igopko1996@gmail.com>
2020-10-11 23:55:31 +0300

    commit

--- script.js
+++ script.js
@@ -1662,3 +1662,40 @@
 // sayHi = null;
 
 // welcome(); // Hello, Guest (вложенный вызов работает)
+
+// counter() должен возвращать следующее значение (как и раньше).
+// counter.set(value) должен устанавливать счётчику значение value.
+// counter.decrease() должен уменьшать значение счётчика на 1.
+// function makeCounter() {
+//     let count = 0;
+//     function counter() {
+//         return count++;
+//     }
+//     counter.set = value => count = value;
+//     counter.decrease = () => count--;
+//     return counter;
+// }
+//
+//
+//
+//
+//сума з обєктів(примітивів)
+//alert( sum(1)(2) ); // 3
+//alert( sum(5)(-1)(2) ); // 6
+//alert( sum(6)(-1)(-2)(-3) )
+
+// function sum(a) {
+//   let currentSum = a;
+//   function f(b) { 
+//     currentSum += b;
+//     return f;
+//   }
+//   f.toString = function() {
+//     return currentSum;
+//   };
+//   return f;
+// }
+// alert( sum(1)(2) ); // 3
+// alert( sum(5)(-1)(2) ); // 6
+// alert( sum(6)(-1)(-2)(-3) ); // 0
+// alert( sum(0)(1)(2)(3)(4)(5) ); // 15
