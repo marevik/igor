5d7f1535fdbdf837f8eaade7d8b5bdb6c0a3950e
Igor <igopko1996@gmail.com>
2020-10-13 23:21:05 +0300

    commit

--- script.js
+++ script.js
@@ -1714,3 +1714,44 @@
 //  Поэтому они не могут использовать внешние локальные переменные. 
 //  Но это очень хорошо, потому что страхует нас от ошибок.
 //  Переданные явно параметры – гораздо лучшее архитектурное решение, которое не вызывает проблем у минификаторов.
+//
+//
+//
+//setTimeout
+// //function sayHi(phrase, who) {
+//   alert( phrase + ', ' + who );
+// }
+// setTimeout(sayHi, 1000, "Привет", "Джон"); // Привет, Джон
+//
+//
+//
+//setTimeout(() => alert('Привет'), 1000);
+//
+//
+//
+//
+//
+// //let timerId = setTimeout(() => alert("ничего не происходит"), 1000);
+// alert(timerId); // идентификатор таймера
+
+// clearTimeout(timerId);
+// alert(timerId); // тот же идентификатор (не принимает значение null после отмены)
+//
+//
+//
+//
+// //// повторить с интервалом 2 секунды
+// let timerId = setInterval(() => alert('tick'), 2000);
+
+// // остановить вывод через 5 секунд
+// setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+//
+// //
+// ///рекурсивний  setTimeout замість   setInterval
+// let timerId = setInterval(() => alert('tick'), 2000);
+// */
+
+// let timerId = setTimeout(function tick() {
+//   alert('tick');
+//   timerId = setTimeout(tick, 2000); // (*)
+// }, 2000);
