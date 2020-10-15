323ce45d8dd357fb16dd092bc6a4631d0051f75b
Igor <igopko1996@gmail.com>
2020-10-15 21:43:59 +0300

    commit

--- script.js
+++ script.js
@@ -1755,3 +1755,38 @@
 //   alert('tick');
 //   timerId = setTimeout(tick, 2000); // (*)
 // }, 2000);
+//
+// //
+// Приклад функция printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to
+// Используем setInterval:
+
+// function printNumbers(from, to) {
+//   let current = from;
+
+//   let timerId = setInterval(function() {
+//     alert(current);
+//     if (current == to) {
+//       clearInterval(timerId);
+//     }
+//     current++;
+//   }, 1000);
+// }
+
+// // использование:
+// printNumbers(5, 10);
+// Используем рекурсивный setTimeout:
+
+// function printNumbers(from, to) {
+//   let current = from;
+
+//   setTimeout(function go() {
+//     alert(current);
+//     if (current < to) {
+//       setTimeout(go, 1000);
+//     }
+//     current++;
+//   }, 1000);
+// }
+
+// // использование:
+// printNumbers(5, 10);
