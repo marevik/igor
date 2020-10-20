63a68ff4d1e14508082053c69e3720a9a90f8948
Igor <igopko1996@gmail.com>
2020-10-20 23:09:01 +0300

    commit

--- script.js
+++ script.js
@@ -1844,3 +1844,55 @@
 // let user = { name: "John" };
 // // 'user' становится 'this', и "Hello" становится первым аргументом
 // say.call( user, "Hello" ); // John: Hello
+//
+//
+//
+//function delay(f, ms) {
+//   return function() {
+//     setTimeout(() => f.apply(this, arguments), ms);
+//   };
+// }
+// let f1000 = delay(alert, 1000);
+// f1000("test"); // показывает "test" после 1000 мс
+//
+//
+//
+//
+//function debounce(f, ms) {
+
+//   let isCooldown = false;
+//   return function() {
+//     if (isCooldown) return;
+//     f.apply(this, arguments);
+//     isCooldown = true;
+//     setTimeout(() => isCooldown = false, ms);
+//   };
+// }
+//
+//
+//
+//
+//function throttle(func, ms) {
+//   let isThrottled = false,
+//     savedArgs,
+//     savedThis;
+
+//   function wrapper() {
+//     if (isThrottled) { // (2)
+//       savedArgs = arguments;
+//       savedThis = this;
+//       return;
+//     }
+
+//     func.apply(this, arguments); // (1)
+//     isThrottled = true;
+//     setTimeout(function() {
+//       isThrottled = false; // (3)
+//       if (savedArgs) {
+//         wrapper.apply(savedThis, savedArgs);
+//         savedArgs = savedThis = null;
+//       }
+//     }, ms);
+//   }
+//   return wrapper;
+// }
