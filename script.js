9c144590f28bd5915d5b6e378ac42e28db84422c
Igor <igopko1996@gmail.com>
2020-10-29 22:23:09 +0200

    commit

--- script.js
+++ script.js
@@ -1985,3 +1985,44 @@
 // user.sayNow("Hello");
 // // Что-то вроде этого:
 // // [10:00] John: Hello!
+
+//
+//
+//
+//
+//-------------стрілкові функції
+// Не имеют this.
+// Не имеют arguments.
+// Не могут быть вызваны с new.
+// let group = {
+//   title: "Our Group",
+//   students: ["John", "Pete", "Alice"],
+
+//   showList() {
+//     this.students.forEach(
+//       student => alert(this.title + ': ' + student)
+//     );
+//   }
+// };
+
+// group.showList();
+//
+//
+//
+//
+//
+
+
+
+// function defer(f, ms) {
+//   return function() {
+//     setTimeout(() => f.apply(this, arguments), ms)
+//   };
+// }
+
+// function sayHi(who) {
+//   alert('Hello, ' + who);
+// }
+
+// let sayHiDeferred = defer(sayHi, 2000);
+// sayHiDeferred("John"); // выводит "Hello, John" через 2 секунды
