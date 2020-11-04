9998ddeb22d4c5c9c5b4f8efc726657eb0ea9f0b
Igor <igopko1996@gmail.com>
2020-11-04 23:28:59 +0200

    commit

--- script.js
+++ script.js
@@ -2200,3 +2200,26 @@
 //     alert(`Inherited: ${prop}`); // Inherited: eats
 //   }
 // }
+
+//
+//
+//приклад 7
+// let hamster = {
+//   stomach: [],
+//   eat(food) {
+//     this.stomach.push(food);
+//   }
+// };
+// let speedy = {
+//   __proto__: hamster,
+//   stomach: []
+// };
+// let lazy = {
+//   __proto__: hamster,
+//   stomach: []
+// };
+// // Шустрый хомяк нашёл еду
+// speedy.eat("apple");
+// alert( speedy.stomach ); // apple
+// // Живот ленивого хомяка пуст
+// alert( lazy.stomach ); // <ничего>
