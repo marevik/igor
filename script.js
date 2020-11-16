10679439893ee7a72d27d90600294c790538731d
Igor <igopko1996@gmail.com>
2020-11-16 23:24:08 +0200

    commit

--- script.js
+++ script.js
@@ -2347,3 +2347,102 @@
 // }
 // // список свойств, разделённых запятой, выведен с помощью toString
 // alert(dictionary); // "apple,__proto__"
+//
+///
+///
+//
+//
+//
+// //========================класи
+//
+//
+//Базовый синтаксис для классов выглядит так:
+// class MyClass {
+//   prop = value; // свойство
+//   constructor(...) { // конструктор
+//     // ...
+//   }
+//   method(...) {} // метод
+//   get something(...) {} // геттер
+//   set something(...) {} // сеттер
+//   [Symbol.iterator]() {} // метод с вычисляемым именем (здесь - символом)
+//   // ...
+// }
+//
+//
+///
+//
+///
+//
+
+// class User {
+//   constructor(name) { this.name = name; }
+//   sayHi() { alert(this.name); }
+// }
+// // класс - это функция
+// alert(typeof User); // function
+// // ...или, если точнее, это метод constructor
+// alert(User === User.prototype.constructor); // true
+// // Методы находятся в User.prototype, например:
+// alert(User.prototype.sayHi); // alert(this.name);
+// // в прототипе ровно 2 метода
+// alert(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
+// //
+// //
+// //
+// //
+// //
+// //
+// class User {
+//   constructor(name) {
+//     // вызывает сеттер
+//     this.name = name;
+//   }
+//   get name() {
+//     return this._name;
+//   }
+//   set name(value) {
+//     if (value.length < 4) {
+//       alert("Имя слишком короткое.");
+//       return;
+//     }
+//     this._name = value;
+//   }
+// }
+// let user = new User("Иван");
+// alert(user.name); // Иван
+// user = new User(""); // Имя слишком короткое.
+//
+//
+//
+//
+// class Clock {
+//   constructor({ template }) {
+//     this.template = template;
+//   }
+//   render() {
+//     let date = new Date();
+//     let hours = date.getHours();
+//     if (hours < 10) hours = '0' + hours;
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = '0' + mins;
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = '0' + secs;
+//     let output = this.template
+//       .replace('h', hours)
+//       .replace('m', mins)
+//       .replace('s', secs);
+//     console.log(output);
+//   }
+
+//   stop() {
+//     clearInterval(this.timer);
+//   }
+//   start() {
+//     this.render();
+//     this.timer = setInterval(() => this.render(), 1000);
+//   }
+// }
+
+// let clock = new Clock({template: 'h:m:s'});
+// clock.start();
