d86e82b66b3e0afb68c15e9e47ffc46e17fb6d61
Igor <igopko1996@gmail.com>
2020-11-18 23:57:34 +0200

    commit

--- script.js
+++ script.js
@@ -2443,6 +2443,38 @@
 //     this.timer = setInterval(() => this.render(), 1000);
 //   }
 // }
-
 // let clock = new Clock({template: 'h:m:s'});
 // clock.start();
+
+//
+//
+
+//
+//
+// class Animal {
+//   constructor(name) {
+//     this.speed = 0;
+//     this.name = name;
+//   }
+//   run(speed) {
+//     this.speed = speed;
+//     alert(`${this.name} бежит со скоростью ${this.speed}.`);
+//   }
+//   stop() {
+//     this.speed = 0;
+//     alert(`${this.name} стоит.`);
+//   }
+
+// }
+// class Rabbit extends Animal {
+//   hide() {
+//     alert(`${this.name} прячется!`);
+//   }
+//   stop() {
+//     super.stop(); // вызываем родительский метод stop
+//     this.hide(); // и затем hide
+//   }
+// }
+// let rabbit = new Rabbit("Белый кролик");
+// rabbit.run(5); // Белый кролик бежит со скоростью 5.
+// rabbit.stop(); // Белый кролик стоит. Белый кролик прячется!
