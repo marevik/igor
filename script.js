b89b9d5f766bbbc4d05c27cb490884903fe6d73a
Igor <igopko1996@gmail.com>
2020-11-30 21:10:26 +0200

    commit

--- script.js
+++ script.js
@@ -2464,7 +2464,6 @@
 //     this.speed = 0;
 //     alert(`${this.name} стоит.`);
 //   }
-
 // }
 // class Rabbit extends Animal {
 //   hide() {
@@ -2520,3 +2519,43 @@
 // let coffeeMachine = new CoffeeMachine(100);
 // // добавляем воды
 // coffeeMachine.waterAmount = 200;
+//
+//
+//
+// //
+// class User {
+//   static staticMethod() {
+//     alert(this === User);
+//   }
+// }
+// User.staticMethod(); // true
+//
+//
+// //
+// class Article {
+//   constructor(title, date) {
+//     this.title = title;
+//     this.date = date;
+//   }
+//   static compare(articleA, articleB) {
+//     return articleA.date - articleB.date;
+//   }
+// }
+// // використання
+// let articles = [
+//   new Article("HTML", new Date(2019, 1, 1)),
+//   new Article("CSS", new Date(2019, 0, 1)),
+//   new Article("JavaScript", new Date(2019, 11, 1))
+// ];
+// articles.sort(Article.compare);
+// alert( articles[0].title ); // CSS
+//
+//
+//
+//
+//
+// //статичні влативості
+// class Article {
+//   static publisher = "Илья Кантор";
+// }
+// alert( Article.publisher ); // Илья Кантор
