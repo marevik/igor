2a9d6cb9175a05aa2b74d8c32770ec6443380b07
Igor <igopko1996@gmail.com>
2021-01-02 22:21:51 +0200

    commit

--- script.js
+++ script.js
@@ -2719,3 +2719,18 @@
 //     alert( "ошибка поймана!" );
 //   }
 // }, 1000);
+//
+//
+//
+//
+//==================колбеки
+// function loadScript(src, callback) {
+//   let script = document.createElement('script');
+//   script.src = src;
+//   script.onload = () => callback(script);
+//   document.head.append(script);
+// }
+// loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+//   alert(`Здорово, скрипт ${script.src} загрузился`);
+//   alert( _ ); // функция, объявленная в загруженном скрипте
+// });
